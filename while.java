248237c26b75253d3614761d295a8105a494e2e6
gunjal-manasi <gunjalmanasi6@gmail.com>
2025-01-08 18:02:44 +0530

    while Loop code

---
+++ while.java
@@ -0,0 +1,12 @@
+
+class WhileEx{
+    public static void main(String args[]){
+        int i=1;
+        while(i<=4)
+        {
+            System.out.println("Hello" + i);
+            i++;
+        }
+        System.out.println("Loop Terminate");
+    }
+}
