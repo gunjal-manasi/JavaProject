4314b757549e173d353591a49bef3952a9752e16
gunjal-manasi <gunjalmanasi6@gmail.com>
2025-01-08 18:31:37 +0530

    If_else code

---
+++ IfElsecode.java
@@ -0,0 +1,14 @@
+public class IfElsecode {
+    public static void main(String args[])
+    {
+        int age=20;
+        if(age>18)
+        {
+            System.out.println("You are valid to vote");
+
+        }
+        else{
+            System.out.println("You arenot valid");
+        }
+    }
+}
