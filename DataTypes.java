b6351bbbbfb312434da2b163b743c015fdd42167
gunjal-manasi <gunjalmanasi6@gmail.com>
2025-01-08 15:07:09 +0530

    Data Types code

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String args[])
+    {
+        byte b=127;
+        int i = 340;
+        long L = 1234l;
+        short s = 34;
+        double d= 2.4;
+        float f = 2.5f;
+        char c= 'M';
+        boolean bool = true;
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(L);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(c);
+    }
+}
